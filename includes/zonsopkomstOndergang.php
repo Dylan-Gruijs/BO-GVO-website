f1242eb78104bb885ff45835967d9a5355814c25
Etienne <40747@ma-web.nl>
2026-06-15 13:50:57 +0200

    zonsopkomstOndergang-Fix

--- includes/zonsopkomstOndergang.php
+++ includes/zonsopkomstOndergang.php
@@ -1,11 +1,22 @@
 <link rel="stylesheet" href="includes/zonsopkomstOndergang.css">
 <?php
 
-$info = date_sun_info(time(), 52.3676, 4.9041);
+date_default_timezone_set('Europe/Amsterdam');
+
+$url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today&formatted=0";
+$response = file_get_contents($url);
+$data = json_decode($response, true);
+
+$sunrise = 'niet beschikbaar';
+$sunset = 'niet beschikbaar';
+if ($data && isset($data['status']) && $data['status'] === 'OK') {
+    $sunrise = date('H:i:s', strtotime($data['results']['sunrise']));
+    $sunset = date('H:i:s', strtotime($data['results']['sunset']));
+}
 
 echo '<div class="sun-card">';
 echo '<h2>Zonsopgang: </h2>';
-echo '<p>' . date('H:i:s', $info['sunrise']) . '</p>';
+echo '<p>' . $sunrise . '</p>';
 echo '<h2>Zonsondergang: </h2>';
-echo '<p>' . date('H:i:s', $info['sunset']) . '</p>';
+echo '<p>' . $sunset . '</p>';
 echo '</div>';
